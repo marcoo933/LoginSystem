1ada9dd498182141493b18a9b2371ed7282f1d51
MP93 <80913365+marcoo933@users.noreply.github.com>
2023-04-16 12:36:53 +0200

    Cors adding to BE

--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -9,7 +10,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class SignupComponent implements OnInit {
   signupForm: FormGroup;
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     this.signupForm = this.createFormGroup();
@@ -24,7 +25,7 @@ export class SignupComponent implements OnInit {
   }
 
   signup(): void {
-    console.log(this.signupForm.value);
+    this.authService.signup(this.signupForm.value).subscribe((msg) => console.log(msg));
   }
 
 }
