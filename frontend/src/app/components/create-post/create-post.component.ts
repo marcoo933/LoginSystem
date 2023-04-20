2a0bb56d74accbfe9017b7619da9b2c50c4d61ec
MP93 <80913365+marcoo933@users.noreply.github.com>
2023-04-20 12:52:05 +0200

    post ui

--- frontend/src/app/components/create-post/create-post.component.ts
+++ frontend/src/app/components/create-post/create-post.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { Post } from 'src/app/models/Post';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-create-post',
@@ -7,5 +9,28 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./create-post.component.scss']
 })
 export class CreatePostComponent {
+  @ViewChild("formDirective") formDirective: NgForm;
+  form: FormGroup;
+
+  constructor(private authService: AuthService) {
+
+  }
+
+  ngOnInit() {
+    this.form = this.createFormGroup();
+  }
+
+  createFormGroup(): FormGroup {
+    return new FormGroup({
+      title: new FormControl("", [Validators.required, Validators.minLength(5)]),
+      body: new FormControl("", [Validators.required, Validators.minLength(10)]),
+    })
+  }
+
+  onSubmit(formData: Pick<Post, "title" | "body">): void {
+    console.log(formData);
+    this.form.reset();
+    this.formDirective.resetForm();
+  }
 
 }
